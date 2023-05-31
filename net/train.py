10774fdc887ae0140545be1ffc1617c26bf10d7e
Tony Okeke <tonykabilanokeke@gmail.com>
2023-05-31 01:12:49 -0400

    Update

--- net/train.py
+++ net/train.py
@@ -2,51 +2,27 @@
 Functions & Classes for Model Training
 """
 
+
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
-from torch import nn
-import pretrainedmodels as ptm
+import pretrainedmodels as models
 import torch
 
 from tqdm.auto import tqdm
 from glob import glob
 import numpy as np
+import copy
+import time
 import re
-
-
-def transform(config):
-    """Tranforms for training, validation, and testing"""
-
-    return transforms.Compose(
-        [
-            transforms.Resize((config["input-height"], config["input-width"])),
-            transforms.ToTensor(),
-            transforms.Normalize(config["input-mean"], config["input-std"]),
-        ]
-    )
-
-
-def feature_dataloaders(config):
-    """Create dataloaders for training, validation, and testing"""
-
-    ds = {
-        "train": datasets.ImageFolder(config["train-dir"], transform(config)),
-        "test": datasets.ImageFolder(config["test-dir"], transform(config)),
-        "val": datasets.ImageFolder(config["val-dir"], transform(config)),
-    }
-
-    return {
-        "train": DataLoader(ds["train"], batch_size=config["batch-size"], shuffle=True),
-        "test": DataLoader(ds["test"], batch_size=config["batch-size"], shuffle=True),
-        "val": DataLoader(ds["val"], batch_size=config["batch-size"], shuffle=False),
-    }
 
 
 def load_ptm(config, replace_last=True):
     """Load a pretrained model and remove the last layer"""
 
-    model = ptm.__dict__[config["model-name"]](num_classes=1000, pretrained="imagenet")
+    model = models.__dict__[config["model-name"]](
+        num_classes=1000, pretrained="imagenet"
+    )
     if replace_last:
         model.last_linear = torch.nn.Identity()
     return model
@@ -57,36 +33,141 @@ def device():
     return torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-class FeatureDataset(Dataset):
-    """
-    Custom dataset to load InceptionV3 features for fine-tuning
-    """
-
-    def __init__(self, feature_dir):
-        self.files = sorted(glob(f"{feature_dir}/FL*.npy"), key=self._extract_idx)
-
-    def __len__(self):
-        return len(self.files)
-
-    def __getitem__(self, idx):
-        label, feature = np.load(self.files[idx], allow_pickle=True)
-        return torch.from_numpy(feature), torch.from_numpy(label)
-
-    def _extract_idx(self, filename):
-        """Extract batch index from filename"""
-        match = re.search(r"(\d+)\.npy$", filename)
-        match = int(match.group(1)) if match else -1
-        if match == -1:
-            raise ValueError(f"Invalid filename {filename}")
-        return match
-
-
-def accuracy(outputs, labels):
-    """Compute accuracy of outputs compared to labels"""
-    _, preds = torch.max(outputs, dim=1)
-    return torch.sum(preds == labels).item() / len(preds)
-
-
 def pbar(dataloader, desc=None):
     """Create a tqdm progress bar"""
-    return tqdm(dataloader, desc=desc, total=len(dataloader), leave=True, disable=False)
+
+    return tqdm(
+        dataloader,
+        desc=desc,
+        total=len(dataloader),
+        leave=False,
+        disable=False,
+        dynamic_ncols=True,
+        bar_format="{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}]",
+    )
+
+
+def inception_loss(outputs, aux_outputs, labels, criterion):
+    """
+    Loss function for training InceptionV3
+
+    Inception is a special case during training because it produces two outputs:
+    - outputs: the final layer
+    - aux_outputs: an intermediate layer
+
+    During training, we calculate the loss by taking a weighted sum of the
+    final output and auxiliary output losses.
+    During testing, validation, and inference, we only consider the final output.
+
+    From https://discuss.pytorch.org/t/how-to-optimize-inception-model-with-auxiliary-classifiers/7958
+    """
+
+    loss1 = criterion(outputs, labels)
+    loss2 = criterion(aux_outputs, labels)
+    return loss1 + 0.4 * loss2
+
+
+def train_model(
+    model,
+    dataloaders,
+    criterion,
+    optimizer,
+    num_epochs=25,
+    epoch_start=0,
+    writer=None,
+    scheduler=None,
+    is_inception=True,
+):
+    """
+    Train a model
+    """
+
+    since = time.time()
+
+    history = {k: {"acc": [], "loss": []} for k in ["train", "val"]}
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(epoch_start, epoch_start + num_epochs):
+        print(f"Epoch {epoch + 1}/{num_epochs}")
+        print("-" * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ["train", "val"]:
+            if phase == "train":
+                model.train()  # Set model to training mode
+            else:
+                model.eval()  # Set model to evaluation mode
+
+            # Track loss, TPs across batches
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data (batches)
+            for inputs, labels in pbar(dataloaders[phase]):
+                # Move tensors to the configured device
+                inputs = inputs.to(device())
+                labels = labels.to(device())
+
+                # Zero the parameter gradients
+                optimizer.zero_grad()
+
+                # Forward pass
+                # track history if only in train
+                with torch.set_grad_enabled(phase == "train"):
+                    # Get model outputs and calculate loss
+                    if is_inception and phase == "train":
+                        outputs, aux_outputs = model(inputs)
+                        loss = inception_loss(outputs, aux_outputs, labels, criterion)
+                    else:
+                        outputs = model(inputs)
+                        loss = criterion(outputs, labels)
+
+                    # Determine predictions
+                    _, preds = torch.max(outputs, 1)
+
+                    # backward + optimize only if in training phase
+                    if phase == "train":
+                        loss.backward()
+                        optimizer.step()
+
+                # Update statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            # Update learning rate
+            if phase == "train" and scheduler is not None:
+                scheduler.step()
+
+            # Calculate epoch statistics
+            N = len(dataloaders[phase].dataset)
+            epoch_loss = running_loss / N
+            epoch_acc = running_corrects.double() / N
+
+            # Update history
+            history[phase]["acc"].append(epoch_acc)
+            history[phase]["loss"].append(epoch_loss)
+
+            # Print epoch statistics
+            print(f"{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}")
+
+            # Write to TensorBoard
+            if writer is not None:
+                writer.add_scalar(f"{phase}/loss", epoch_loss, epoch)
+                writer.add_scalar(f"{phase}/acc", epoch_acc, epoch)
+
+            # Deep copy the model
+            if phase == "val" and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        print()
+
+    time_elapsed = time.time() - since
+    print(f"Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s")
+    print(f"Best val Acc: {best_acc:4f}")
+
+    # Load best model weights
+    model.load_state_dict(best_model_wts)
+    return model, history
