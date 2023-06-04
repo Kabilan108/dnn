f28a8bfc11c4a129b82935e1ef29a92b69a1d16d
Tony Okeke <tonykabilanokeke@gmail.com>
2023-06-04 19:10:24 -0400

    Add handling for ctrl+C

--- net/data.py
+++ net/data.py
@@ -10,16 +10,20 @@ def download_xray():
     Download the paultimothymooney/chest-xray-pneumonia dataset from kaggle
     """
 
-    subprocess.run(
-        """
-        kaggle datasets download -d paultimothymooney/chest-xray-pneumonia
-        unzip -q "chest-xray-pneumonia.zip" -d "data"
-        rm -r "chest-xray-pneumonia.zip" "data/chest_xray/__MACOSX"
-        rm -r "data/chest_xray/chest_xray"
-    """,
-        shell=True,
-        check=True,
-    )
+    try:
+        subprocess.run(
+            """
+            kaggle datasets download -d paultimothymooney/chest-xray-pneumonia
+            unzip -q "chest-xray-pneumonia.zip" -d "data"
+            rm -r "chest-xray-pneumonia.zip" "data/chest_xray/__MACOSX"
+            rm -r "data/chest_xray/chest_xray"
+        """,
+            shell=True,
+            check=True,
+        )
+    except KeyboardInterrupt:
+        print("Aborted")
+        return
 
     return
 
@@ -29,17 +33,21 @@ def download_oct():
     Download the paultimothymooney/kermany2018 dataset from kaggle
     """
 
-    subprocess.run(
-        """
-        kaggle datasets download -d paultimothymooney/kermany2018
-        unzip -q "kermany2018.zip" -d "data/oct"
-        mv data/oct/OCT2017\ /t* data/oct && mv data/oct/OCT2017\ /v* data/oct
-        rmdir data/oct/OCT2017\ /
-        rm -r "data/oct/oct2017" "kermany2018.zip"
-    """,
-        shell=True,
-        check=True,
-    )
+    try:
+        subprocess.run(
+            """
+            kaggle datasets download -d paultimothymooney/kermany2018
+            unzip -q "kermany2018.zip" -d "data/oct"
+            mv data/oct/OCT2017\ /t* data/oct && mv data/oct/OCT2017\ /v* data/oct
+            rmdir data/oct/OCT2017\ /
+            rm -r "data/oct/oct2017" "kermany2018.zip"
+        """,
+            shell=True,
+            check=True,
+        )
+    except KeyboardInterrupt:
+        print("Aborted")
+        return
 
     return
 
