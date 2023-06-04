f834770492d5d98dd99d3954f13eae70a1472f70
Tony Okeke <tonykabilanokeke@gmail.com>
2023-06-04 18:19:10 -0400

    Remove redundant functions

--- net/utils.py
+++ net/utils.py
@@ -12,19 +12,6 @@ import torchvision.transforms as transforms
 import torch
 
 
-def create_directory(path):
-    """Create directory if it does not exist"""
-    if not os.path.exists(path):
-        os.makedirs(path)
-
-
-def get_image_files(imagedir):
-    """Return a sorted list of image files in a directory"""
-    return sorted(
-        [os.path.basename(fname) for fname in glob.glob(f"{imagedir}/*.jpeg")]
-    )
-
-
 def plot_roc(y_test, y_score, pos_label=0):
     """
     Plot a Receiver Operating Characteristic (ROC) curve and return the Area
@@ -63,44 +50,6 @@ def plot_roc(y_test, y_score, pos_label=0):
     plt.ylim((0.0, 1.05))
 
     return auc_roc, (fig, ax)
-
-
-def save_model(model, path):
-    """Save trained model (.pth) to path"""
-    torch.save(model.state_dict(), path)
-
-
-def create_image_lists(image_dir):
-    """
-    Take in image directory, return a dictionary containing images split into
-    training, testing and validation splits
-    """
-    image_lists = {}
-    label_dirs = glob.glob(os.path.join(image_dir, "*"))
-    for label_dir in label_dirs:
-        label_name = os.path.basename(label_dir)
-        image_lists[label_name] = {
-            "training": get_image_files(os.path.join(label_dir, "train")),
-            "testing": get_image_files(os.path.join(label_dir, "test")),
-            "validation": get_image_files(os.path.join(label_dir, "validation")),
-        }
-    return image_lists
-
-
-def get_image_path(image_lists, label_name, index, image_dir, category):
-    image_file = image_lists[label_name][category][index]
-    return os.path.join(image_dir, label_name, category, image_file)
-
-
-# Return path to bottleneck for a given label and a given index
-def get_bottleneck_path(image_lists, label_name, index, bottleneck_dir, category):
-    bottleneck_file = (
-        os.path.splitext(
-            get_image_path(image_lists, label_name, index, bottleneck_dir, category)
-        )[0]
-        + ".pt"
-    )
-    return bottleneck_file
 
 
 def plot_history(history):
